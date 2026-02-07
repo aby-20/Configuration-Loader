0e3851c72b0305b5f2b6e76d1915c11eac9a8339
aby-20 <abysyril1919@gmail.com>
2026-02-07 10:05:53 +0000

    Added the parser validator for bool also

--- valid.c
+++ valid.c
@@ -37,4 +37,26 @@ return 1;
 
 }
 
+int parse_bool(const char* value,int *out){
 
+if (value == NULL) return 0;
+if (out == NULL) return 0;
+
+
+
+if(strcmp(value,"true")==0){
+*out = 1;
+return 1;}
+else if(strcmp(value,"false") == 0){
+*out = 0;
+return 1;}
+else if(strcmp(value,"1") == 0){
+*out = 1;
+return 1;}
+else if(strcmp(value,"0") == 0){
+*out = 0;
+return 1;}
+
+return 0;
+
+}
