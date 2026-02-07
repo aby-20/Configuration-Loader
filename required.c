1538d20b09636085398000ab6b668832ac0eec4a
aby-20 <abysyril1919@gmail.com>
2026-02-07 11:56:13 +0000

    config: enforce required configuration keys

---
+++ required.c
@@ -0,0 +1,9 @@
+int validate_required(cfg_schema* schema,size_t schema_count){
+for(size_t i=0 ; i<schema_count;i++){
+if(schema[i].required ==1 && schema[i].seen == 0)
+{
+return 0;
+}
+}
+return 1;
+}
